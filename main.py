bfd999e6cee0902fdc4bb74f77ea581e64862b0e
Daniel <danboy2k1@gmail.com>
2020-04-18 22:23:26 -0400

    Fixed recursion error
    Algorithm complete

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import sys
 import random
 sys.setrecursionlimit(2000)
 
+rounds = 3
+
 class Players:
     def __init__(self, name):
         self.name = name
@@ -21,48 +23,37 @@ p8 = Players("Mike")
 
 players = [p1, p2, p3, p4, p5, p6, p7, p8]
 
-def firstRound():
+def playerSort():
     random.shuffle(players)
-    y = 0
-    for x in players:
-        if y % 2 == 0:
-            players[y].addPlayer(players[y + 1])
-            y+=1
-        else:
-            players[y].addPlayer(players[y - 1])
-            y+=1
-
-def roundUp():
-    random.shuffle(players)
-    y = 0
-    for x in players:
-        if y % 2 == 0:
-            if players[y + 1] in players[y].playedWith:
-                roundUp()
+    for x in range(rounds):
+        y = 0
+        while y < len(players):
+            if y % 2 == 0:
+                if players[y + 1] in players[y].playedWith:
+                    random.shuffle(players)
+                    y = 0
+                else:
+                    y+=1
             else:
+                if players[y - 1] in players[y].playedWith:
+                    random.shuffle(players)
+                    y = 0
+                else:
+                    y+=1
+        y = 0
+        for x in players:
+            if y % 2 == 0:
+                players[y].addPlayer(players[y + 1])
                 y+=1
-        else:
-            if players[y - 1] in players[y].playedWith:
-                roundUp()
+
             else:
+                players[y].addPlayer(players[y - 1])
                 y+=1
-    y = 0
-    for x in players:
-        if y % 2 == 0:
-            players[y].addPlayer(players[y + 1])
-            y+=1
 
-        else:
-            players[y].addPlayer(players[y - 1])
-            y+=1
+playerSort()
 
 
 
-
-
-
-firstRound()
-roundUp()
 
 
 
